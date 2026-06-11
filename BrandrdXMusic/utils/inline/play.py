5bc6648a1c5be9dd1230f07660ea7aeac4286a67
RONEY THE BRAND <nikhilkohli928877@gmail.com>
2026-06-11 13:00:41 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -67,7 +67,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url="https://t.me/Bindas_life0"),
+            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url="https://t.me/Cute_Panditt"),
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url="https://t.me/RONEY_CHAT_GROUP"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -85,7 +85,7 @@ def stream_markup(_, videoid, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url="https://t.me/Bindas_life0"),
+            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url="https://t.me/Cute_Panditt"),
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url="https://t.me/RONEY_CHAT_GROUP"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
